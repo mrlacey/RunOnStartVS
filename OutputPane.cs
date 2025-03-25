726b79c86d7deea824896671a1e9d36ef36fce11
Matt Lacey <matt@mrlacey.co.uk>
2025-03-24 23:08:32 -0700

    Initial code implementation

---
+++ OutputPane.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace RunOnStartVS;
+
+public class OutputPane
+{
+	private static Guid outputPaneGuid = new Guid("D7697023-CB76-4CCB-849B-4C5119EAABD4");
+
+	private static OutputPane instance;
+
+	private readonly IVsOutputWindowPane pane;
+
+	private OutputPane()
+	{
+		ThreadHelper.ThrowIfNotOnUIThread();
+
+		if (ServiceProvider.GlobalProvider.GetService(typeof(SVsOutputWindow)) is IVsOutputWindow outWindow
+			&& (ErrorHandler.Failed(outWindow.GetPane(ref outputPaneGuid, out this.pane)) || this.pane == null))
+		{
+			if (ErrorHandler.Failed(outWindow.CreatePane(ref outputPaneGuid, Vsix.Name, 1, 0)))
+			{
+				System.Diagnostics.Debug.WriteLine("Failed to create output pane.");
+				return;
+			}
+
+			if (ErrorHandler.Failed(outWindow.GetPane(ref outputPaneGuid, out this.pane)) || (this.pane == null))
+			{
+				System.Diagnostics.Debug.WriteLine("Failed to get output pane.");
+			}
+		}
+	}
+
+	public static OutputPane Instance => instance ?? (instance = new OutputPane());
+
+	public async Task ActivateAsync()
+	{
+		await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(CancellationToken.None);
+
+		this.pane?.Activate();
+	}
+
+	public async Task WriteLineAsync(string message)
+	{
+		await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(CancellationToken.None);
+
+		this.pane?.OutputStringThreadSafe($"{message}{Environment.NewLine}");
+	}
+}
---
+++ VSPackage.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using EnvDTE;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace RunOnStartVS;
+
+[ProvideAutoLoad(Microsoft.VisualStudio.Shell.Interop.UIContextGuids.SolutionHasMultipleProjects, PackageAutoLoadFlags.BackgroundLoad)]
+[ProvideAutoLoad(Microsoft.VisualStudio.Shell.Interop.UIContextGuids.SolutionHasSingleProject, PackageAutoLoadFlags.BackgroundLoad)]
+[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+[InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)] // Info on this package for Help/About
+[Guid(VSPackage.PackageGuidString)]
+public sealed class VSPackage : AsyncPackage
+{
+	/// <summary>
+	/// VSPackage GUID string.
+	/// </summary>
+	public const string PackageGuidString = "e885b52c-90a6-463b-95af-b8c6200c46c3";
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="VSPackage"/> class.
+	/// </summary>
+	public VSPackage()
+	{
+	}
+
+	protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+	{
+		// When initialized asynchronously, the current thread may be a background thread at this point.
+		// Do any initialization that requires the UI thread after switching to the UI thread.
+		await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+		await OutputPane.Instance.WriteLineAsync($"{Vsix.Name} v{Vsix.Version}");
+
+		// Since this package might not be initialized until after a solution has finished loading,
+		// we need to check if a solution has already been loaded and then handle it.
+		bool isSolutionLoaded = await this.IsSolutionLoadedAsync(cancellationToken);
+
+		if (isSolutionLoaded)
+		{
+			await this.HandleOpenSolutionAsync(cancellationToken);
+		}
+
+		// Listen for subsequent solution events
+		Microsoft.VisualStudio.Shell.Events.SolutionEvents.OnAfterOpenSolution += this.HandleOpenSolution;
+	}
+
+	private async Task<bool> IsSolutionLoadedAsync(CancellationToken cancellationToken)
+	{
+		await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+		if (!(await this.GetServiceAsync(typeof(SVsSolution)) is IVsSolution solService))
+		{
+			throw new ArgumentNullException(nameof(solService));
+		}
+
+		ErrorHandler.ThrowOnFailure(solService.GetProperty((int)__VSPROPID.VSPROPID_IsSolutionOpen, out object value));
+
+		return value is bool isSolOpen && isSolOpen;
+	}
+
+	private void HandleOpenSolution(object sender, EventArgs e)
+	{
+		this.JoinableTaskFactory.RunAsync(() => this.HandleOpenSolutionAsync(this.DisposalToken)).Task.LogAndForget("DemoSnippets");
+	}
+
+	private async Task HandleOpenSolutionAsync(CancellationToken cancellationToken)
+	{
+		await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+		var powerShellFileName = "run-on-startup.ps1";
+
+		// Get all *.demosnippets files from the solution
+		// Do this now for performance and to avoid thread issues
+		if (await this.GetServiceAsync(typeof(DTE)) is DTE dte)
+		{
+			var fileName = dte.Solution.FileName;
+
+			if (!string.IsNullOrWhiteSpace(fileName) && File.Exists(fileName))
+			{
+				var slnDir = Path.GetDirectoryName(fileName);
+
+				var powerShellFilePath = Path.Combine(slnDir, powerShellFileName);
+
+				if (File.Exists(powerShellFilePath))
+				{
+					await OutputPane.Instance.WriteLineAsync($"Running '{powerShellFilePath}'");
+
+					using (var process = new System.Diagnostics.Process())
+					{
+						process.StartInfo = new ProcessStartInfo
+						{
+							FileName = "powershell.exe",
+							Arguments = $"-File {powerShellFilePath}",
+							WorkingDirectory = slnDir,
+							UseShellExecute = false,
+							CreateNoWindow = true,
+							RedirectStandardOutput = true,
+							RedirectStandardError = true,
+						};
+
+						try
+						{
+							process.Start();
+
+							var result = await process.StandardOutput.ReadToEndAsync();
+							var error = await process.StandardError.ReadToEndAsync();
+							process.WaitForExit();
+
+							await OutputPane.Instance.WriteLineAsync(result);
+							await OutputPane.Instance.WriteLineAsync(error);
+							await OutputPane.Instance.WriteLineAsync($"ExitCode: {process.ExitCode}");
+						}
+						catch (Exception exc)
+						{
+							await OutputPane.Instance.WriteLineAsync("Error running PowerShell script.");
+							await OutputPane.Instance.WriteLineAsync(exc.Message);
+						}
+					}
+
+				}
+				else
+				{
+					await OutputPane.Instance.WriteLineAsync($"No startup file found. Looked for '{powerShellFilePath}'");
+				}
+			}
+			else
+			{
+				await OutputPane.Instance.WriteLineAsync($"Error: Could not access solution file to use to find {powerShellFileName} file.");
+			}
+		}
+		else
+		{
+			await OutputPane.Instance.WriteLineAsync("Error: Unable to search for file to run.");
+		}
+
+		await OutputPane.Instance.ActivateAsync();
+	}
+}
